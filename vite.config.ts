9cf609496ccc08c7ee3303d252eb4e09d8954533
sp <2849872658@qq.com>
2024-04-10 09:28:08 +0800

    build: 更新打包设置添加压缩

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueDevTools from 'vite-plugin-vue-devtools'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
+import { visualizer } from 'rollup-plugin-visualizer'
+import viteCompression from 'vite-plugin-compression'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,6 +20,15 @@ export default defineConfig({
     }),
     Components({
       resolvers: [ElementPlusResolver()]
+    }),
+    visualizer(),
+    viteCompression({
+      verbose: true, // 输出压缩成功
+      disable: false, // 是否禁用
+      threshold: 10240, // 体积大于阈值会被压缩，单位是b
+      algorithm: 'gzip', // 压缩算法
+      ext: '.gz', // 生成的压缩包后缀
+      deleteOriginFile: true // 是否删除原文件
     })
   ],
   resolve: {
